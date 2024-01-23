ebaac38f12be34b01a4f812236da2650bcbcc977
Ilya Karpovs <karpov.ilja@gmail.com>
2024-01-23 20:51:02 +0200

    Specification

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 <img width="749" alt="Screenshot 2024-01-23 at 14 46 02" src="https://github.com/19th/circus/assets/637672/9717d713-6b1c-4203-8831-482c8f3621f6">
 
 **Specifikācija**
-- 1pt Spelētāji sāk no lauciņa nr. 1, vispār 100 lauciņu. Vinē tas kurš pirmais sasniedz pēdējo lauciņu
+- 1pt Spelētāji sāk no lauciņa nr. 1, vispār 100 lauciņu. Ir divi spēlētāji. Vinē tas kurš pirmais sasniedz pēdējo lauciņu
 - 1pt Maksimāli - 25 raundi, ja beidzas raundi - neizšķirts
-- 1pt Viens pēc otra met kauliņu un iet uz priekšu
+- 1pt Viens pēc otra met kauliņu (ar nejauša ciparu ģenerātora palidzību) un iet uz priekšu
 - 1pt Ja trāpa uz lauciņu ar kāpnem:
 zilas kāpnes ved uz leju
 18 -> 7, 67 -> 46 , 80 -> 69, 74 -> 63
---
+++ main.py
@@ -0,0 +1,25 @@
+# Specifikācija
+# - 1pt Spelētāji sāk no lauciņa nr. 1, vispār 100 lauciņu. Ir divi spēlētāji. Vinē tas kurš pirmais sasniedz pēdējo lauciņu
+# - 1pt Maksimāli - 25 raundi, ja beidzas raundi - neizšķirts
+# - 1pt Viens pēc otra met kauliņu (ar nejauša ciparu ģenerātora palidzību) un iet uz priekšu
+# - 1pt Ja trāpa uz lauciņu ar kāpnem:
+# -- zilas kāpnes ved uz leju, 18 -> 7, 67 -> 46 , 80 -> 69, 74 -> 63
+# -- sarkanas kāpnes ved uz augšu, 15 -> 24, 39 -> 48, 33 -> 52, 87 -> 96 
+# - 1pt Katrā raundā tik drukāta informācija kur atrodas spēlētājs, dators un vai ir uzkāpts uz kāpnem
+
+# Koda vertēšanas kritēriji
+# - 1pt Kodā izmanto mainīgus, ciklus (for vai while), zarošanu (if)
+# - 1pt Kods strādā bez kļūdam
+# - 1pt Mainīgo un funkciju nosaukumi atspoguļo izmantošanas būtību, bez saisinājumiem, rakstīti snake_case stilā
+# - 1pt Kodam ir jēdzīgi komentāri, pirms "if, for" koda konstrukcijam
+# - 1pt Izmaiņas saglabātas versiju vadības sistēmā Git
+
+# Dokumentācija
+# Mainīgie - https://www.w3schools.com/python/python_variables.asp
+# Operācijas ar mainīgiem - https://www.w3schools.com/python/python_operators.asp
+# Mainīgo drukāšana - https://www.w3schools.com/python/python_variables_output.asp
+# Nosacījumi, zarošana, if ... else - https://www.w3schools.com/python/python_conditions.asp
+# For cikls - https://www.w3schools.com/python/python_for_loops.asp
+# Nejauša skaitļa generēšana - https://www.w3schools.com/python/ref_random_randint.asp
+
+
